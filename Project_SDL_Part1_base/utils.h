4458731880f2f591897cf2fe06cb3e5666b03891
Samy Abdouche <samy.abdouche@gmail.com>
2021-10-27 11:16:53 +0200

    renamed files

---
+++ Project_SDL_Part1_base/utils.h
@@ -0,0 +1,21 @@
+﻿
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+
+// Definitions
+constexpr double frame_rate = 60.0; // refresh rate
+constexpr double frame_time = 1. / frame_rate;
+constexpr unsigned frame_width = 1400; // Width of window in pixel
+constexpr unsigned frame_height = 900; // Height of window in pixel
+// Minimal distance of animals to the border
+// of the screen
+constexpr unsigned frame_boundary = 100;
+
+
+
+// Helper function to initialize SDL
+void init();
+SDL_Surface* load_surface_for(const std::string& path, SDL_Surface* window_surface_ptr);
