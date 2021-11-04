3399530a1a5373e756203cbd9694f6f3700a3ca1
Samy Abdouche <samy.abdouche@gmail.com>
2021-11-04 18:08:44 +0100

    modified update method

--- Project_SDL_Part1_base/ground.cpp
+++ Project_SDL_Part1_base/ground.cpp
@@ -2,9 +2,9 @@
 // Created by xplo on 27/10/2021.
 //
 
-#include "headers.h"
 #include "ground.h"
 #include "animal.h"
+#include "headers.h"
 
 ground::ground() : animals_{std::vector<std::shared_ptr<animal>>()}{}
 
@@ -16,32 +16,46 @@ std::vector<std::shared_ptr<animal>> ground::getAnimals() const {
   return animals_;
 }
 
-void ground::update() {
-    auto new_sheeps = std::vector<std::shared_ptr<animal>>();
-for (const auto& a : animals_) {
-    int xA = a->pos_x() + (a->getw()) / 2;
-    int yA = a->pos_y() + (a->geth()) / 2;
-    for (const auto& b : animals_) {
+void ground::update(){
+
+
+  std::shared_ptr<animal> a, b, c;
+
+  for (int i = 0; i < animals_.size(); ++i){
+
+    a = animals_[i];
+    double xA = a->pos_x() + (a->getw()) / 2;
+    double yA = a->pos_y() + (a->geth()) / 2;
+
+    if (a->hasprop("wolf")){
+
+      for (int j = 0; j < animals_.size(); ++j){
+
+        b = animals_[j];
         if (a == b)
-            continue;
-        double xB = b->pos_x()+(b->getw())/2;
+          continue;
+
+        //calcul de la distance
+        double xB = b->pos_x() + (b->getw()) / 2;
         double yB = b->pos_y() + (b->geth()) / 2;
         double dx = xB - xA;
         double dy = yB - yA;
         double distance = sqrt(dx * dx + dy * dy);
-        if (distance < 50) {
-            
-            a->interact(b.get(),*this,new_sheeps);
-        }
-        
-            
+
+          if (distance < 50)
+            a->interact(b, *this);
+      }
     }
-    for (auto s : new_sheeps)
-        animals_.push_back(s);
-}
-  for (const auto& a : animals_) {
-    a->move();
-    a->draw();
   }
+  for (auto &c : animals_) {
+    c->move();
+    c->draw();
+  }
+
 }
 
+
+
+std::vector<std::shared_ptr<animal>>& ground::getAnimals() {
+  return animals_;
+}
