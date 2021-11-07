394edf495eefe87464305b4cf8e05280421993d6
Samy Abdouche <samy.abdouche@gmail.com>
2021-11-07 20:32:59 +0100

    animal

--- Project_SDL_Part1_base/animal.cpp
+++ Project_SDL_Part1_base/animal.cpp
@@ -6,65 +6,8 @@
 #include "animal.h"
 #include "ground.h"
 
-constexpr unsigned int distanceFromMommy = 50;
 
-animal::animal(const std::string &file_path, SDL_Surface *window_surface_ptr)
-    : moving_object(window_surface_ptr, load_surface_for(file_path, window_surface_ptr), {"adult"}) {}
-
-void animal::draw() const{
-
-  if (window_surface_ptr_ == nullptr || image_ptr_ == nullptr){
-    std::cout << "pointers aren't ok, bro, do something";
-    return;
-  }
-
-  //These two next lines are used to get rid of the PNG backgrounds
-  Uint32 color_key = SDL_MapRGB(image_ptr_->format, 0, 0, 0);
-  SDL_SetColorKey(image_ptr_, SDL_TRUE, color_key);
-  SDL_Rect pos;
-  pos.x = (int)pos_x_;
-  pos.y = (int)pos_y_;
-  pos.h = image_ptr_->h;
-  pos.w = image_ptr_->w;
-  SDL_BlitScaled(image_ptr_, nullptr, window_surface_ptr_, &pos);
-}
+animal::animal(const std::string &file_path, SDL_Surface *window_surface_ptr, ground& ground_ptr)
+    : moving_object(window_surface_ptr, load_surface_for(file_path, window_surface_ptr), {"adult"}), ground_ptr_{ground_ptr} {}
 
 
-void animal::copyMommyProperties(animal* mom){
-  pos_x_ = mom->pos_x_ + distanceFromMommy;
-  pos_y_ = mom->pos_y_ + distanceFromMommy;
-  vel_x_ = mom->vel_x_;
-  vel_y_ = mom->vel_y_;
-
-  int mom_w = ceil((mom->image_ptr_->w)/1.5);
-  int mom_h = ceil((mom->image_ptr_->h)/1.5);
-
-  setDimensions(mom_w, mom_h);
-}
-
-
-
-std::chrono::time_point<std::chrono::system_clock>& animal::getTimer(){
-    return timer_;
-}
-void animal::move() {
-    constrained_linear_move_(pos_x(), pos_y(), vel_x(), vel_y());
-}
-
-animal* animal::whoIsFemale(const std::shared_ptr<animal>& otherObject) {
-  if (otherObject->hasProperty("female"))
-    return (otherObject.get());
-
-  else if (hasProperty("female"))
-    return (this);
-  else
-    throw std::runtime_error("This shouldn't have happened at all.\n");
-}
-
-bool animal::canMakeBabies(const std::shared_ptr<animal> &otherObject) {
-  if (this->hasProperty("female") && otherObject->hasProperty("male") || this->hasProperty("female") && otherObject->hasProperty("male"))
-    if (!this->hasProperty("lamb") &&  otherObject->hasProperty("lamb"))
-      return (true);
-
-  return (false);
-}
--- Project_SDL_Part1_base/animal.h
+++ Project_SDL_Part1_base/animal.h
@@ -2,31 +2,16 @@
 // Created by xplo on 27/10/2021.
 //
 #pragma once
-#include "headers.h"
 #include "moving_object.h"
-#include "declarations.hh"
-
 
 
 class animal: public moving_object {
 protected:
-  //some animal behaviors are defined by time constraints. Therefore,
-  //we'll use some sort of timer to implement said constraints.
-  std::chrono::time_point<std::chrono::system_clock> timer_;
+  ground& ground_ptr_;
 
 public:
-  animal(const std::string& file_path, SDL_Surface* window_surface_ptr);
+  animal(const std::string& file_path, SDL_Surface* window_surface_ptr, ground& ground_ptr);
   virtual ~animal() = default;
 
-  void draw() const;
-
-  void move() override;
-
-  [[nodiscard]] std::chrono::time_point<std::chrono::system_clock>& getTimer();
-
-  animal* whoIsFemale(const std::shared_ptr<animal>& otherObject);
-  bool canMakeBabies(const std::shared_ptr<animal>& otherObject);
-
-  void copyMommyProperties(animal* mom);
 
 };
