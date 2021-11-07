8cc96f3cc6d3e0eb5652652212ccfc86e702b064
Samy Abdouche <samy.abdouche@gmail.com>
2021-11-07 20:35:13 +0100

    added headers

--- Project_SDL_Part1_base/headers.h
+++ Project_SDL_Part1_base/headers.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "Project_SDL1.h"
+#include "SDL_ttf.h"
 #include <string>
 #include <SDL.h>
 #include <SDL_image.h>
@@ -14,4 +15,15 @@
 #include <algorithm>
 #include <chrono>
 #include <list>
+#include <random>
 
+constexpr SDL_Color black = {0, 0, 0};
+constexpr unsigned int childhoodDuration = 5;
+constexpr std::string_view samy_sheep_path = "/home/xplo/ESIEE/cpp/Project_SDL_Part1_ABDOUCHE/media/sheep.png";
+constexpr std::string_view samy_lamb_path = "/home/xplo/ESIEE/cpp/Project_SDL_Part1_ABDOUCHE/media/lamb.png";
+constexpr std::string_view samy_wolf_path = "/home/xplo/ESIEE/cpp/Project_SDL_Part1_ABDOUCHE/media/wolf.png";
+constexpr unsigned int sexFreeDuration = 10;
+constexpr unsigned int starvingDuration = 3;
+constexpr unsigned int distanceFromMommy = 50;
+constexpr std::string_view anh_my_sheep_path = "./media/sheep.png";
+constexpr std::string_view anh_my_wolf_path = "./media/wolf.png";
